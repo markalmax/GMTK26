e071e6ca3caad7f9a6faa7fc342d2ab5ae48053b
markalmax <markalmax782@gmail.com>
2026-07-23 20:11:55 +0300

    Camera and mouse controls (thx boxnbox)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,5 +8,12 @@ public class Player : Actors
         float XInput = Input.GetAxis("Horizontal");
         float YInput = Input.GetAxis("Vertical");
         Move(new Vector2(XInput, YInput));
+        Vector2 vector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = (vector - (Vector2)transform.position).normalized;
+        vector= new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 vector2 = Camera.main.ScreenToWorldPoint(vector);
+        vector2 -= base.transform.position;
+        float num = Mathf.Atan2(vector2.y, vector2.x) * 57.29578f - 90f;
+        transform.rotation = Quaternion.Euler(0f, 0f, num);
     }
 }
