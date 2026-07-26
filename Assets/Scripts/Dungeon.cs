b79eda7cc89e5d907fca3bc529f12a92aa1265ec
markalmax <markalmax782@gmail.com>
2026-07-26 10:38:28 +0300

    small improvements

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Dungeon : MonoBehaviour
 {
-    public GameObject room;
+    public GameObject roomPrefab;
+    public GameObject[] rooms;
     public Vector2 minimumSize,maximumSize;
     [Tooltip("Percent of rooms in grid")][Range(0, 100)]
     public int roomDensity;
     [Tooltip("The space between the center of rooms")]
     public float roomSpace;
-    public List<List<int>> dungeon;
+    public bool[] dungeon;
+    private int n,m;
 
     public void Start()
     {
@@ -21,28 +23,41 @@ public class Dungeon : MonoBehaviour
     public void GenerateDungeon()
     {
         //map size
-        int n,m;
         n=Random.Range((int)minimumSize.x,(int)maximumSize.x+1);
         m=Random.Range((int)minimumSize.y,(int)maximumSize.y+1);
         Debug.Log(n+"   "+m);
-        //create rooms
+        rooms = new GameObject[n * m];
+        dungeon = new bool[n * m];
         for(int i = 0; i < n; i++)
         {
             for(int j=0; j < m; j++)
             {
                 if (Random.Range(0, 100)<roomDensity)
-                {
-
-                    //dungeon[i][j]=1;
-
-                    Instantiate(room, new Vector3(i*roomSpace,j*roomSpace,0),Quaternion.identity);
+                {                 
+                    dungeon[pos(i,j)]=true;
+                    Debug.Log(pos(i,j));                      
+                    rooms[pos(i,j)] = Instantiate(roomPrefab, new Vector3(i*roomSpace,j*roomSpace,0),Quaternion.identity);
                 }
-                else
+                else                     
                 {
-                    //dungeon[i][j]=0;
-
+                    dungeon[pos(i,j)]=false;
                 }    
             }
         }
     }
+    [ContextMenu("DeleteDungeon")]
+    public void DeleteDungeon()
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j=0; j < m; j++)
+            {
+                if(rooms[pos(i,j)]!=null){
+                    Destroy(rooms[pos(i,j)]);
+                    rooms[pos(i,j)] = null;
+                }
+            }
+        }    
+    }
+    int pos(int i,int j)=>i*m+j;    
 }
